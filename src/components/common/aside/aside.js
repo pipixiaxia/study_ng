03e634fc35cc8ce23feaa4d59c186f6c915e98aa
pipixia <pipixia@pipi.com>
2017-08-31 19:12:40 +0800

    基础项目结构

---
+++ src/components/common/aside/aside.js
@@ -0,0 +1,12 @@
+angular.module("aside", [])
+    .directive("nglAside", [function () {
+        return {
+            scope: {},
+            restrict: "E",
+            templateUrl: "/src/components/common/aside/aside.html",
+            replace: true,
+            controller: ["$scope", function ($scope) {
+                console.log("侧边栏");
+            }]
+        }
+    }]);
